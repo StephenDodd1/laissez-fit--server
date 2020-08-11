e2d217784e8215911e0222a1b2d8b3509df33c96
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-10 20:51:49 -0500

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
    PORT: process.env.PORT || 8000,
    NODE_ENV: process.env.NODE_ENV || 'development',
-   CLIENT_ORIGIN: "https://laissez-fit-client.vercel.app/"
+   CLIENT_ORIGIN: "https://laissez-fit-client.vercel.app/",
+   DATABASE_URL: "postgresql://postgres@localhost/laissez_fit"
  }
