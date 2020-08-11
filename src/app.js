edbf46e2510ffc0b50e0db50e56263c14e76c1c0
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-10 20:37:37 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -3,16 +3,20 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+
+const { NODE_ENV, CLIENT_ORIGIN, DATABASE_URL } = require('./config')
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
-
+const knex = require('knex')
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors({
+   origin: CLIENT_ORIGIN
+}))
 app.get('/', (req, res) => {
    res.send('Hello, world!')
 })
@@ -26,6 +30,8 @@ app.use(function errorHandler(error, req, res, next) {
    }
    res.status(500).json(response)
 })
-app.use(cors())
 
+app.get('*', function(req, res, next) {
+   if (req.url === '/') return next();
+ });
 module.exports = app
