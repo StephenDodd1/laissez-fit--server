ffc7d5662a5d3b972c9fab1431a3242d6f45f9d2
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-20 12:23:46 -0500

    debugging

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://laissez-fit-client.vercel.app/",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://laissez-fit-client.vercel.app",
   DATABASE_URL: process.env.DATABASE_URL || "postgres://fganolsibrybdd:a9a33dfb3c8d92f86b4fa09e64ee74fa225b553a61fe222292c32a0119350581@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d3njn0mlkldsl5" /*|| "postgresql://postgres@localhost/laissez_fit"*/,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: 10000
