8ed66121865fbb982bebfb0ae7c5e0e1f03e6e22
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-20 12:05:14 -0500

    debugging -- deleted and reinstalled all dependencies.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000", //"https://laissez-fit-client.vercel.app/",
-  DATABASE_URL: "postgresql://postgres@localhost/laissez_fit",/*process.env.DATABASE_URL,*/
+  DATABASE_URL: process.env.DATABASE_URL || "postgres://fganolsibrybdd:a9a33dfb3c8d92f86b4fa09e64ee74fa225b553a61fe222292c32a0119350581@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d3njn0mlkldsl5" /*|| "postgresql://postgres@localhost/laissez_fit"*/,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: 10000
  }
