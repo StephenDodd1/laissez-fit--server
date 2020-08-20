8381b9ff18f15e57692b9f7d829603a8899229b2
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-19 20:24:00 -0500

    update postgrator

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
   CLIENT_ORIGIN: "http://localhost:3000" || "https://laissez-fit-client.vercel.app/",
-  DATABASE_URL: "postgresql://postgres@localhost/laissez_fit",
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: 10000
  }
