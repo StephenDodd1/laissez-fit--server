523525e4fe25e8aff4ee293beca0e6fb3375be22
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-20 20:15:33 -0500

    debugging

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -2,7 +2,6 @@ const UsersService = {
   authenticateUser(knex, username, password){
     return knex('users')
       .where({'username': username})
-      .andWhere({'password': password})
       .first()
   },
   createUser(knex, user){
