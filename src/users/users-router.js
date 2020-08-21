90e0e5733685e6c9770abfa0dbd6c16a572312a0
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-20 20:31:30 -0500

    debugging

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -31,7 +31,9 @@ usersRouter.route("/api/user").post(jsonBodyParser, (req, res, next) => {
       if (!user || user.password !== tokenPassword) {
         console.log('!user ran')
         return res.status(401).json({ error: "Unauthorized request" });
-      } else {const jwtToken = createAuthToken(user);
+      } else {
+        console.log('jwtToken is running')
+        const jwtToken = createAuthToken(user);
       const data = {jwtToken, user}
       console.log(data)
       return res.status(202).json({ data })};
