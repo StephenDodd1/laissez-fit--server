6adf521e3b510de307e87fd72af6413511587e3e
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-20 20:42:44 -0500

    debugging

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://laissez-fit-client.vercel.app",
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/laissez_fit" /*|| "postgresql://postgres@localhost/laissez_fit"*/,
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: 10000
+  JWT_EXPIRY: 7d
  }
